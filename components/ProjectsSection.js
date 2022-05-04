4bd96827b96c6f71eb5bfa1433d3a36c6d3b87ce
Tim Vo <timtvo714@gmail.com>
2022-05-03 18:56:30 -0700

    projects data

--- components/ProjectsSection.js
+++ components/ProjectsSection.js
@@ -1,28 +1,69 @@
 import React from 'react';
+import projects from '../data/projects';
+
+const Item = ({imageSrc}) => {
+  return (
+    <div className="relative mb-4 w-full md:w-1/2 lg:w-1/3 px-4">
+      <div className="relative h-80 mb-5 mx-auto rounded-lg">
+        <img
+          className="h-80 w-full relative h-full rounded-lg object-cover"
+          src={imageSrc}
+          alt=""
+        />
+        {/**hover component */}
+        <div className="opacity-0 hover:opacity-100 ">
+
+          <div className="absolute inset-0 bg-purple-600 opacity-75 rounded-lg" />
+          <div className="absolute inset-0 p-6 flex flex-col items-start">
+            <span className="text-purple-400">2021</span>
+            <p className="mb-auto text-xl lg:text-2xl text-white font-bold">
+              Lorem ipsum dolor sit amet consectutar
+            </p>
+            <a className="inline-block py-2 px-4 border-2 border-purple-400 hover:border-white bg-transparent text-gray-50 hover:bg-white hover:text-gray-900 transition duration-200 rounded-l-xl rounded-t-xl font-bold leading-loose">
+              View Project
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const TopSkew = () => {
+  return (
+    <div className="skew skew-top ml-for-radius">
+      <svg
+        className="h-8 md:h-12 lg:h-20 w-full text-gray-900 "
+        viewBox="0 0 10 10"
+        preserveAspectRatio="none"
+      >
+        <polygon fill="currentColor" points="0 10 10 0 10 10" />
+      </svg>
+    </div>
+  );
+};
+
+const BottomSkew = () => {
+  return (
+    <div className="skew skew-bottom mr-for-radius">
+      <svg
+        className="h-8 md:h-12 lg:h-20 w-full text-gray-900"
+        viewBox="0 0 10 10"
+        preserveAspectRatio="none"
+      >
+        <polygon fill="currentColor" points="0 0 10 0 0 10" />
+      </svg>
+    </div>
+  );
+};
 
 const ProjectsSection = () => {
+  const {trickify, mugbucket, gizmos} = projects;
   return (
     <section className="skewed-top-left skewed-bottom-right bg-gray-50">
-      <div className="skew skew-top mr-for-radius ">
-        <svg
-          className="h-8 md:h-12 lg:h-20 w-full text-gray-900 "
-          viewBox="0 0 10 10"
-          preserveAspectRatio="none"
-        >
-          <polygon fill="currentColor" points="0 0 10 10 0 10" />
-        </svg>
-      </div>
-      <div className="skew skew-top ml-for-radius">
-        <svg
-          className="h-8 md:h-12 lg:h-20 w-full text-gray-900 "
-          viewBox="0 0 10 10"
-          preserveAspectRatio="none"
-        >
-          <polygon fill="currentColor" points="0 10 10 0 10 10" />
-        </svg>
-      </div>
 
-      <div className="py-20 bg-gray-900 radius-for-skewed">
+      <TopSkew />
+      <div className="py-20 bg-gray-900 radius-for-skewed min-h-[150vh]">
         <div className="container px-4 mx-auto">
           <div className="mb-16 flex flex-wrap justify-center md:justify-between items-center">
             <div className="text-center lg:text-left">
@@ -30,104 +71,25 @@ const ProjectsSection = () => {
                 Dolor sit amet consectutar
               </span>
               <h2 className="text-4xl lg:text-5xl font-bold font-heading text-white">
-                Featured Projects
+                My Projects
               </h2>
             </div>
-            <a className="hidden md:inline-block py-2 px-6 rounded-l-xl rounded-t-xl bg-purple-600 hover:bg-purple-700 text-gray-50 font-bold leading-loose transition duration-200">
-              View More Projects
-            </a>
+
           </div>
           <div className="flex flex-wrap -mx-4 mb-4">
-            <div className="w-full md:w-1/2 lg:w-1/3 mb-8 px-4">
-              <a>
-                <img
-                  className="h-80 w-full mx-auto object-cover rounded"
-                  src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-                  alt=""
-                />
-              </a>
-            </div>
-            <div className="relative mb-4 w-full md:w-1/2 lg:w-1/3 px-4">
-              <div className="relative h-80 mb-5 mx-auto rounded-lg">
-                <img
-                  className="h-80 w-full relative h-full rounded-lg object-cover"
-                  src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1055&amp;q=80"
-                  alt=""
-                />
-                <div className="absolute inset-0 bg-purple-600 opacity-75 rounded-lg" />
-                <div className="absolute inset-0 p-6 flex flex-col items-start">
-                  <span className="text-purple-400">2021</span>
-                  <p className="mb-auto text-xl lg:text-2xl text-white font-bold">
-                    Lorem ipsum dolor sit amet consectutar
-                  </p>
-                  <a className="inline-block py-2 px-4 border-2 border-purple-400 hover:border-white bg-transparent text-gray-50 hover:bg-white hover:text-gray-900 transition duration-200 rounded-l-xl rounded-t-xl font-bold leading-loose">
-                    View Project
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/3 mb-8 px-4">
-              <a>
-                <img
-                  className="h-80 w-full mx-auto object-cover rounded"
-                  src="https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=968&amp;q=80"
-                  alt=""
-                />
-              </a>
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/3 mb-8 px-4">
-              <a>
-                <img
-                  className="h-80 w-full mx-auto object-cover rounded"
-                  src="https://images.unsplash.com/photo-1476610182048-b716b8518aae?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1127&amp;q=80"
-                  alt=""
-                />
-              </a>
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/3 mb-8 px-4">
-              <a>
-                <img
-                  className="h-80 w-full mx-auto object-cover rounded"
-                  src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-                  alt=""
-                />
-              </a>
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/3 mb-8 px-4">
-              <a>
-                <img
-                  className="h-80 w-full mx-auto object-cover rounded"
-                  src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-                  alt=""
-                />
-              </a>
-            </div>
+
+            <Item imageSrc={mugbucket.imageSrc} />
+
+            <Item imageSrc={trickify.imageSrc} />
+
+            <Item imageSrc={gizmos.imageSrc} />
+
           </div>
-          <div className="text-center">
-            <a className="md:hidden inline-block py-2 px-6 rounded-l-xl rounded-t-xl bg-purple-600 hover:bg-purple-700 text-gray-50 font-bold leading-loose transition duration-200">
-              View More Projects
-            </a>
-          </div>
+
         </div>
       </div>
-      <div className="skew skew-bottom mr-for-radius">
-        <svg
-          className="h-8 md:h-12 lg:h-20 w-full text-gray-900"
-          viewBox="0 0 10 10"
-          preserveAspectRatio="none"
-        >
-          <polygon fill="currentColor" points="0 0 10 0 0 10" />
-        </svg>
-      </div>
-      <div className="skew skew-bottom ml-for-radius">
-        <svg
-          className="h-8 md:h-12 lg:h-20 w-full text-gray-900"
-          viewBox="0 0 10 10"
-          preserveAspectRatio="none"
-        >
-          <polygon fill="currentColor" points="0 0 10 0 10 10" />
-        </svg>
-      </div>
+      <BottomSkew />
+
     </section>
   );
 };
---
+++ data/projects.js
@@ -0,0 +1,22 @@
+const projects = {
+  trickify: {
+    name: 'Trickify',
+    desc: 'Bruhhh',
+    url: '',
+    imageSrc: 'https://res.cloudinary.com/ddj5orpun/image/upload/v1651624141/screencapture-localhost-3000-test-2022-05-03-17_26_27_kzfuoy.png',
+  },
+  mugbucket: {
+    name: 'Mug Bucket',
+    desc: 'Bruhhh',
+    url: '',
+    imageSrc: 'https://res.cloudinary.com/ddj5orpun/image/upload/v1647129209/mug-bucket-camera_nionsj.jpg',
+  },
+  gizmos: {
+    name: 'Gizmos',
+    desc: 'Bruhhh',
+    url: '',
+    imageSrc: 'https://res.cloudinary.com/ddj5orpun/image/upload/v1651626584/screencapture-localhost-3000-test-2022-05-03-18_09_27_ramiib.png',
+  },
+};
+
+export default projects;
