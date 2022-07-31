52067a19a1d9be390e6c8da19fed79f43d623503
Tim Vo <timtvo714@gmail.com>
2022-07-31 16:45:50 -0700

    test1

--- components/ImageOverlay.js
+++ components/ImageOverlay.js
@@ -4,7 +4,7 @@ import lines from "../assets/elements/lines.svg";
 
 const ImageOverlay = () => {
   return (
-    <div className="w-full h-[105vh] absolute -mt-[13rem] left-0">
+    <div className="w-full h-[105vh] absolute -mt-[15rem] left-0">
       <Image
         src={lines}
         className="w-[120%] object-cover clip-custom"
