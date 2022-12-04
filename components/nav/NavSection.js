d3c02c53700ad817fa5035c20079b5b5ccddeec3
Tim Vo <timtvo714@gmail.com>
2022-12-03 19:43:55 -0800

    remove resume link for now...

--- components/nav/NavSection.js
+++ components/nav/NavSection.js
@@ -105,7 +105,7 @@ const NavSection = () => {
               Contact
             </ScrollLink>
           </li>
-          <li className="text-gray-300">
+          {/* <li className="text-gray-300">
             <svg
               className="w-4 h-4 current-fill"
               xmlns="http://www.w3.org/2000/svg"
@@ -120,12 +120,12 @@ const NavSection = () => {
                 d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
               />
             </svg>
-          </li>
-          <li>
+          </li> */}
+          {/* <li>
             <button className="text-sm text-gray-400 hover:text-purple-500 font-bold cursor-pointer">
               Resume
             </button>
-          </li>
+          </li> */}
         </ul>
       </nav>
       {/* <div
